5eba3a0ae7fc992086d6a84e8dd1f02d38aa9680
Guillaume Raymond <guiray@users.noreply.github.com>
2017-02-17 14:13:13 -0500

    all works with a timer, simplified code

--- Irradiance_arduino/Irradiance_arduino.ino
+++ Irradiance_arduino/Irradiance_arduino.ino
@@ -1,44 +1,38 @@
 // Analog 1 UVA
-float A1Value = 0;
-float A1oldSensorReading = 0;
-float A1SensorReading;
-
+int A1SensorReading;
 // Analog 2 Pyranometer (will change to light sensor)
-float A2Value = 0;
-float A2oldSensorReading = 0;
-float A2SensorReading;
-
+int A2SensorReading;
 // Digital 1 Radiation Monitor
-//int D1pin = 2;
+int D1pin = 2;
+
+unsigned long time;
 
 void setup() {
-  //pinMode(2, INPUT);
+  pinMode(2, INPUT);
   Serial.begin(9600);
-  Serial.println("Serial Ready");
+  //Serial.println("Serial Ready");
 }
 
 void loop() {
-   
-  // Analog 1
-  A1oldSensorReading = A1SensorReading;
-  A1SensorReading = analogRead(A0);
-  if (A1SensorReading != A1oldSensorReading) {
-    //Serial.print("0");
-    //Serial.println(A1SensorReading);
-  }
+  time = millis();
 
-  // Analog 2
-  A2oldSensorReading = A2SensorReading;
-  A2SensorReading = analogRead(A2);
-  if (A2SensorReading != A2oldSensorReading) {
-    //Serial.print("2");
-    //Serial.println(A2SensorReading);
+  if (time % 100 == 0) {
+    A1SensorReading = analogRead(A0);
+    Serial.print("A1_");
+    Serial.println(A1SensorReading);
+
+    A2SensorReading = analogRead(A2);
+    Serial.print("A2_");
+    Serial.println(A2SensorReading);
+
+    delay(10);
   }
 
   // Digital 1
-//  if (digitalRead(D1pin) == 1) {
-//    Serial.println("1");
-//    delay(1);
-//  }
+  if (digitalRead(D1pin) == 1) {
+    Serial.print("D1_");
+    Serial.println("1");
+    delay(1);
+  }
 
 }
