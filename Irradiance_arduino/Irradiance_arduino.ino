5ba23cb41ae4401e44bc7ba75453f82c22da69c7
Guillaume Raymond <guiray@users.noreply.github.com>
2017-02-17 00:11:03 -0500

    too much time wasted, only analog it is!

--- Irradiance_arduino/Irradiance_arduino.ino
+++ Irradiance_arduino/Irradiance_arduino.ino
@@ -9,10 +9,10 @@ float A2oldSensorReading = 0;
 float A2SensorReading;
 
 // Digital 1 Radiation Monitor
-int D1pin = 2;
+//int D1pin = 2;
 
 void setup() {
-  pinMode(2, INPUT);
+  //pinMode(2, INPUT);
   Serial.begin(9600);
   Serial.println("Serial Ready");
 }
@@ -20,25 +20,25 @@ void setup() {
 void loop() {
    
   // Analog 1
-  //A1oldSensorReading = A1SensorReading;
+  A1oldSensorReading = A1SensorReading;
   A1SensorReading = analogRead(A0);
-  //if (A1SensorReading != A1oldSensorReading) {
+  if (A1SensorReading != A1oldSensorReading) {
     //Serial.print("0");
     //Serial.println(A1SensorReading);
-  //}
+  }
 
   // Analog 2
-  //A2oldSensorReading = A2SensorReading;
-  //A2SensorReading = analogRead(A2);
-  //if (A2SensorReading != A2oldSensorReading) {
+  A2oldSensorReading = A2SensorReading;
+  A2SensorReading = analogRead(A2);
+  if (A2SensorReading != A2oldSensorReading) {
     //Serial.print("2");
     //Serial.println(A2SensorReading);
-  //}
+  }
 
   // Digital 1
-  if (digitalRead(D1pin) == 1) {
-    Serial.println("1");
-    delay(1);
-  }
+//  if (digitalRead(D1pin) == 1) {
+//    Serial.println("1");
+//    delay(1);
+//  }
 
 }
