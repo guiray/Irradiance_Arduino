99337592d3cb8759f0e113f67b531c3fca0d66a7
Guillaume Raymond <guiray@users.noreply.github.com>
2017-02-16 23:49:23 -0500

    digital works prise 2

--- Irradiance_arduino/Irradiance_arduino.ino
+++ Irradiance_arduino/Irradiance_arduino.ino
@@ -1,11 +1,9 @@
 // Analog 1 UVA
-// Values
 float A1Value = 0;
 float A1oldSensorReading = 0;
 float A1SensorReading;
 
-// Analog 2 Pyranometer
-// Values
+// Analog 2 Pyranometer (will change to light sensor)
 float A2Value = 0;
 float A2oldSensorReading = 0;
 float A2SensorReading;
@@ -14,37 +12,33 @@ float A2SensorReading;
 int D1pin = 2;
 
 void setup() {
-  pinMode(A0, INPUT);
-  //pinMode(A2, INPUT);
   pinMode(2, INPUT);
   Serial.begin(9600);
   Serial.println("Serial Ready");
-  
 }
 
 void loop() {
+   
+  // Analog 1
+  //A1oldSensorReading = A1SensorReading;
+  //A1SensorReading = analogRead(A0);
+  //if (A1SensorReading != A1oldSensorReading) {
+    //Serial.print("0");
+    //Serial.println(A1SensorReading);
+  //}
+
+  // Analog 2
+  //A2oldSensorReading = A2SensorReading;
+  //A2SensorReading = analogRead(A2);
+  //if (A2SensorReading != A2oldSensorReading) {
+    //Serial.print("2");
+    //Serial.println(A2SensorReading);
+  //}
+
   // Digital 1
   if (digitalRead(D1pin) == 1) {
     Serial.println("1");
     delay(1);
-  }
-  
-  // Analog 1
-  A1oldSensorReading = A1SensorReading;
-  //A1SensorReading = analogRead(A0);
-  //print on serial port when Value Changes
-  if (A1SensorReading != A1oldSensorReading) {
-    //Serial.print("0");
-    //Serial.println(A1SensorReading);
-  }
-
-  // Analog 2
-  A2oldSensorReading = A2SensorReading;
-  //A2SensorReading = analogRead(A2);
-  // print on serial port when Value Changes
-  if (A2SensorReading != A2oldSensorReading) {
-    //Serial.print("2");
-    //Serial.println(A2SensorReading);
   }
 
 }
