a1dfe6e7b2b2e4ef31e3faafed51e0a4fe83d31d
Guillaume Raymond <guiray@users.noreply.github.com>
2017-02-16 23:26:27 -0500

    digital sensor works 100%

--- Irradiance_arduino/Irradiance_arduino.ino
+++ Irradiance_arduino/Irradiance_arduino.ino
@@ -1,58 +1,50 @@
 // Analog 1 UVA
-// Calibration
-float A1Intercept = 0;
-float A1Slope = 3940;
 // Values
 float A1Value = 0;
 float A1oldSensorReading = 0;
-float A1Voltage;
 float A1SensorReading;
 
 // Analog 2 Pyranometer
-// Calibration
-float A2Intercept = 0;
-float A2Slope = 250;
 // Values
 float A2Value = 0;
 float A2oldSensorReading = 0;
-float A2Voltage;
 float A2SensorReading;
 
 // Digital 1 Radiation Monitor
 int D1pin = 2;
-int countSum = 0;
 
 void setup() {
+  pinMode(A0, INPUT);
+  //pinMode(A2, INPUT);
+  pinMode(2, INPUT);
   Serial.begin(9600);
   Serial.println("Serial Ready");
+  
 }
 
 void loop() {
   // Digital 1
-  if (digitalRead(D1pin)!=0) {
+  if (digitalRead(D1pin) == 1) {
     Serial.println("1");
+    delay(1);
   }
   
   // Analog 1
   A1oldSensorReading = A1SensorReading;
-  A1Value = analogRead(A0);
-  A1Voltage = A1Value / 1023 * 5.0; // convert to raw voltage
-  A1SensorReading = A1Intercept + A1Voltage * A1Slope; // convert voltage to sensor reading
-  // print on serial port when Value Changes
+  //A1SensorReading = analogRead(A0);
+  //print on serial port when Value Changes
   if (A1SensorReading != A1oldSensorReading) {
-    //Serial.print("Analog 1: ");
-    //Serial.println(A1Value);
+    //Serial.print("0");
+    //Serial.println(A1SensorReading);
   }
 
   // Analog 2
   A2oldSensorReading = A2SensorReading;
-  A2Value = analogRead(A2);
-  A2Voltage = A2Value / 1023 * 5.0; // convert to raw voltage
-  A2SensorReading = A2Intercept + A2Voltage * A2Slope; // convert voltage to sensor reading
+  //A2SensorReading = analogRead(A2);
   // print on serial port when Value Changes
   if (A2SensorReading != A2oldSensorReading) {
-    //Serial.print("Analog 2: ");
-    //Serial.println(A2Value);
+    //Serial.print("2");
+    //Serial.println(A2SensorReading);
   }
-    
+
 }
